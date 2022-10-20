99a6c4d72377393567266617e8f3ce43173032c2
Ruben Hernandez Garcia <113932253+rwifislax@users.noreply.github.com>
2022-10-20 03:26:59 -0600

    Update cmdobfuscator.py

--- cmdobfuscator.py
+++ cmdobfuscator.py
@@ -2,6 +2,16 @@
 
 import random
 
+def random_words():
+    alphabet = "abcdefghijklmnopqrstuvwxyz0123456789"
+    random_word = ""
+    n = random.randint(5,10)
+    for i in range(n):
+        random_number = random.randint(0,35)
+        random_word = random_word + alphabet[random_number]
+
+    return random_word
+
 def builder(string):
     #shuffle string and get numbers
     shuffled_string = ''.join(random.sample(string, len(string)))
@@ -20,14 +30,27 @@ def builder(string):
 
     numbers_list = numbers_list + str(random_number)
 
+    #get random words for the variables
+    var1 = random_words()
+    var2 = random_words()
+    
     #build cmd one liner
-    cmd = 'cmd /V /C "set test=' + shuffled_string + '&&for %A in (' + numbers_list + ') do set word=!word!!test:~%A,1!&&if %A==' + str(random_number) + ' call %word:~-' + str(string_length) + '%"'
+    cmd = 'cmd /V /C "set ' + var1 + '=' + shuffled_string + '&&for %A in (' + numbers_list + ') do set ' + var2 + '=!' + var2 + '!!' + var1 + ':~%A,1!&&if %A==' + str(random_number) + ' call %' + var2 + ':~-' + str(string_length) + '%"'
     return cmd
 
 def main():
-
+    
     print("******** CMD Command obfuscator ********\nEnter CMD command to obfuscate.\nPress Enter when done.")
     while True:
+        string = input("Command: ")
+        if string == "":
+            break
+            
+        result = builder(string)
+        print(result)
+
+if __name__ == '__main__':
+    main()
         string = input("Command: ")
         if string == "":
             break
